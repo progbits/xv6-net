07b310705eee4029e9ebe0eb750c1e9a5fec4e33
progbits <53880655+progbits@users.noreply.github.com>
2022-10-22 22:29:15 +0100

    Very basic enumeration of pci devices

--- syspci.c
+++ syspci.c
@@ -2,4 +2,34 @@
 // PCI system calls
 //
 
-int sys_lspci(void) { return 0; }
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+
+int sys_lspci(void) {
+  const short PCI_CONFIG_ADDR = 0xCF8;
+  const short PCI_CONFIG_DATA = 0xCFC;
+
+  // Enumerate the first 4 pci buses.
+  for (int bus = 0; bus < 4; bus++) {
+    cprintf("pci_bus: %d\n", bus);
+    long addr = 0x80000000 | bus << 11;
+
+    cprintf("vendor_id: 0x");
+    for (int i = 1; i >= 0; i--) {
+      outdw(PCI_CONFIG_ADDR, addr | i);
+      uchar data = inb(PCI_CONFIG_DATA);
+      cprintf("%x", data);
+    }
+    cprintf("\n");
+
+    cprintf("device_id: 0x");
+    for (int i = 3; i >= 2; i--) {
+      outdw(PCI_CONFIG_ADDR, addr | i);
+      uchar data = inb(PCI_CONFIG_DATA);
+      cprintf("%x", data);
+    }
+    cprintf("\n");
+  }
+  return 0;
+}
