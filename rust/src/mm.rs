0211a0cabfdb40818a243328d97436999b04c907
progbits <53880655+progbits@users.noreply.github.com>
2022-12-22 14:42:02 +0000

    Add very minimal memory management routines

---
+++ rust/src/mm.rs
@@ -0,0 +1,49 @@
+///
+/// Memory management.
+///
+/// The facilities provided here are currently fairly minimal until we have
+/// all of the memory management code written in Rust.
+///
+
+// These constants currently have to match the values in memlayout.h
+const EXTMEM: u32 = 0x100000; // Start of extended memory.
+const PHYSTOP: u32 = 0xE000000; // Top physical memory.
+const DEVSPACE: u32 = 0xFE000000; // Other devices are at high addresses.
+const KERNBASE: u32 = 0x80000000; // First kernel virtual address.
+const KERNLINK: u32 = KERNBASE + EXTMEM; // Address where kernel is linked.
+
+/// A physical memory addess.
+pub struct PhysicalAddress(u32);
+
+impl PhysicalAddress {
+    /// TODO: Sanity checks.
+    pub fn new(addr: u32) -> PhysicalAddress {
+        PhysicalAddress(addr)
+    }
+
+    pub fn to_virtual_address(&self) -> VirtualAddress {
+        VirtualAddress(self.0 + KERNBASE)
+    }
+
+    pub fn value(&self) -> u32 {
+        self.0
+    }
+}
+
+/// A virtual memory addess.
+pub struct VirtualAddress(u32);
+
+impl VirtualAddress {
+    /// TODO: Sanity checks.
+    pub fn new(addr: u32) -> VirtualAddress {
+        VirtualAddress(addr)
+    }
+
+    pub fn to_physical_address(&self) -> PhysicalAddress {
+        PhysicalAddress(self.0 - KERNBASE)
+    }
+
+    pub fn value(&self) -> u32 {
+        self.0
+    }
+}
