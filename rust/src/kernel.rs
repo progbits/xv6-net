8be215bdf0bf4239c52180f646b68860e49aa0a8
progbits <53880655+progbits@users.noreply.github.com>
2022-12-23 02:16:54 +0000

    Add FFI binding for ioapicenable(...)

--- rust/src/kernel.rs
+++ rust/src/kernel.rs
@@ -2,6 +2,9 @@ use core::ffi::{c_uchar, c_void};
 
 // Bindings to the existing xv6 kernel library.
 extern "C" {
+    // ioapic.c
+    pub fn ioapicenable(irq: u32, cpu: u32);
+
     // console.c
     pub fn cprint(c: *const c_uchar);
 
