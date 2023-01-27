e929864b96f0fda4999fe96c278796a81fa9bf8a
progbits <53880655+progbits@users.noreply.github.com>
2023-01-27 22:05:59 +0000

    Remove buggy UDP checksum calculation

--- rust/src/udp.rs
+++ rust/src/udp.rs
@@ -52,23 +52,7 @@ impl ToBuffer for UdpPacket {
         buf[0..2].copy_from_slice(&self.source_port.to_be_bytes());
         buf[2..4].copy_from_slice(&self.dest_port.to_be_bytes());
         buf[4..6].copy_from_slice(&self.len.to_be_bytes());
-
-        let mut checksum_data = Vec::new();
-        checksum_data.extend(&self.source_port.to_be_bytes());
-        checksum_data.extend(&self.dest_port.to_be_bytes());
-        checksum_data.extend(&self.len.to_be_bytes());
-        checksum_data.extend(&[0, 17]); // Protocol: 17 for UDP
-        checksum_data.extend(&[0, self.data.len() as u8, (self.data.len() >> 8) as u8]);
-        checksum_data.extend(&self.data[..]);
-
-        let checksum = !(checksum_data
-            .chunks(2)
-            .map(|x| u16::from_be_bytes([x[0], x[1]]))
-            .sum::<u16>()
-            + (checksum_data.len() / 2) as u16)
-            & 0xFFFF;
-
-        buf[6..8].copy_from_slice(&checksum.to_be_bytes());
+        buf[6..8].copy_from_slice(&self.checksum.to_be_bytes());
         buf[8..8 + self.data.len()].copy_from_slice(&self.data[..]);
     }
 
